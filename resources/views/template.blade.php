d55a093eecc10a3841d12518ef9e2496658a9004
Rodrigo Ferreira <r.caneppele@gmail.com>
2016-03-04 11:25:43 -0300

    adicionando mensagens de erro no template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,6 +13,16 @@
 
 <body>
 	<div class="container">
+		@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{$error}}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+
 		@yield('conteudo')
 	</div>
 
